c48dc7536852ce1b4408092ad195f711ee03ca05
Kannan Ramakrishnan <kannaa.ram@gmail.com>
2014-09-30 21:13:26 +0530

    Testcases firefox issue

--- features/support/env.rb
+++ features/support/env.rb
@@ -119,3 +119,9 @@ end
 # After do
 #   DeferredGarbageCollection.reconsider
 # end
+
+Capybara.register_driver :selenium do |app|
+  require 'selenium/webdriver'
+  Selenium::WebDriver::Firefox::Binary.path = "/opt/firefox17/firefox"
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
